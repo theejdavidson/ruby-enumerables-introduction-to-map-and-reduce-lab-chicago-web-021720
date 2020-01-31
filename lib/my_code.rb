09810c10c75f2be81f423d52b3a3c3d2e7d8da8b
Ethan Davidson <theejdavidson@gmail.com>
2020-01-31 21:07:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,9 @@ def reduce_to_all_true(source_array)
   result = true 
   index = 0 
   while index < source_array.length do
-    if()
+    if(source_array[index] == nil || source_array[index == false])
+      return false
+    end
     index += 1 
   end 
   return result
