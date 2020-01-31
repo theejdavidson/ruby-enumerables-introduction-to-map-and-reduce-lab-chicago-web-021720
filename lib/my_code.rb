8952989ec207a143f0ca641a7abaddc2bae312c4
Ethan Davidson <theejdavidson@gmail.com>
2020-01-31 20:58:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,4 +40,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
+  total = 0 
+  while starting_point < source_array.length do
+    total += source_array[starting_point]
+    starting_point += 1 
+  end
+  return total
+end
   
