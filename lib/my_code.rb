83e27b2b40a7e195259a8ad0b29ef44809f15e7c
Ethan Davidson <theejdavidson@gmail.com>
2020-01-31 20:50:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+def map_to_negativize(source_array)
+  mapped = []
+  index = 0
+  while index < source_array.length do
+    mapped << (source_array[index] * -1)
+    index += 1
+  end
+  return mapped
+end
